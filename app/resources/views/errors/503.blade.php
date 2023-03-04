86b12a4fdfaa2b410bf78d39316355342e729361
nDoped <lespaul366@gmail.com>
2023-03-03 21:53:56 -0500

    Adds error blades

---
+++ app/resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __("Maintenance"))
+
+@section('code', '503')
+@section('message', __("I'm adding something cool and we'll be back in a second...bare with me"))
