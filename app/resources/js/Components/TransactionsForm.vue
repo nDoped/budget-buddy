c599b8a86cab0d4147b3ad5782df08f7655edbd6
nDoped <lespaul366@gmail.com>
2023-03-18 14:21:21 -0400

    Autofocus amount on trans form

--- app/resources/js/Components/TransactionsForm.vue
+++ app/resources/js/Components/TransactionsForm.vue
@@ -88,7 +88,6 @@
     categoriesInvalid.value = true;
     form.categories = [];
   };
-
 </script>
 
 <template>
@@ -125,6 +124,7 @@
                   min="0"
                   step="any"
                   v-model="form.amount"
+                  autofocus
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
                 <InputError
@@ -208,7 +208,6 @@
                   v-model="form.note"
                   type="text"
                   class="mt-1 block w-full"
-                  autofocus
                   autocomplete="note"
                 />
                 <InputError
