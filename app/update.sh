ba489b5ae1943e3d369906089bbb3401b7dc514a
nDoped <lespaul366@gmail.com>
2024-07-03 01:40:09 -0400

    Some tweaks to update.sh

--- app/update.sh
+++ app/update.sh
@@ -2,15 +2,23 @@
 
 echo "Stopping the app"
 php artisan down
+echo "Creating database dump"
+
+
+if [ ! -d "./database-dumps" ]; then
+    mkdir "./database-dumps"
+fi
+file_date=$(date +%F_%H_%M_%S)
+sudo mysqldump  mykickass_db > "database-dumps/${file_date}_mykickass_db.sql"
 
 echo "Pulling updates"
-sudo git pull --rebase
+git pull --rebase
 
 echo " composer install"
 php composer.phar install
 
 echo "npm install"
-sudo npm install
+npm install
 
 echo "Building"
 npm run build
@@ -19,13 +27,13 @@ echo "Running migrations"
 php artisan migrate
 
 function cache_and_start_server {
-        echo "Refreshing caches"
-        php artisan config:cache
-        php artisan view:cache
-        php artisan route:cache
+    echo "Refreshing caches"
+    php artisan config:cache
+    php artisan view:cache
+    php artisan route:cache
 
-        echo "Starting the app"
-        php artisan up
+    echo "Starting the app"
+    php artisan up
 }
 
 trap cache_and_start_server EXIT
