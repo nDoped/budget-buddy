4b4c6bcc734897ba06653e37c833947c05382e05
nDoped <lespaul366@gmail.com>
2024-07-13 02:12:56 -0400

    Creates ToggleSlider component

---
+++ app/resources/js/Components/ToggleSlider.vue
@@ -0,0 +1,26 @@
+<script setup>
+  defineEmits([ 'update:modelValue' ]);
+  const model = defineModel();
+  defineProps({
+    label: {
+      type: String,
+      default: 'Turn it on'
+    },
+  });
+</script>
+
+<template>
+  <div class="bg-slate-500 inline-block rounded">
+    <label class="relative flex justify-start items-center p-2 text-l">
+      <input
+        type="checkbox"
+        v-model="model"
+        class="absolute left-1/2 -translate-x-1/2 opacity-0 w-full h-full peer appearance-none rounded-md"
+      >
+      <span
+        class="w-16 h-5 m-2 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-3 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6"
+      />
+      {{ label }}
+    </label>
+  </div>
+</template>
--- app/resources/js/Components/TransactionCategory.vue
+++ app/resources/js/Components/TransactionCategory.vue
@@ -10,6 +10,7 @@
   import InputError from '@/Components/InputError.vue';
   import CategoryInputs from '@/Components/CategoryInputs.vue';
   import CategorySelect from '@/Components/CategorySelect.vue';
+  import ToggleSlider from '@/Components/ToggleSlider.vue';
   import PrimaryButton from '@/Components/PrimaryButton.vue';
   import DangerButton from '@/Components/DangerButton.vue';
 
@@ -266,19 +267,10 @@
 
 <template>
   <div>
-    <div class="bg-slate-500 inline-block rounded">
-      <label class="relative flex justify-start items-center p-2 text-l">
-        <input
-          type="checkbox"
-          v-model="calcCatsByReciept"
-          class="absolute left-1/2 -translate-x-1/2 opacity-0 w-full h-full peer appearance-none rounded-md"
-        >
-        <span
-          class="w-16 h-5 m-2 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-3 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6"
-        />
-        Enter receipt line items
-      </label>
-    </div>
+    <ToggleSlider
+      v-model="calcCatsByReciept"
+      label="Enter receipt line items"
+    />
 
     <template v-if="! calcCatsByReciept">
       <div class="flex flex-col md:flex-row content-between dark:bg-slate-500">
