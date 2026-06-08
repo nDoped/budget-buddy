4e4e2445fe19905d93942d802c0547ffd886bd60
nDoped <lespaul366@gmail.com>
2026-06-08 18:30:31 -0400

    Updates laravel to 12.x

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -4,7 +4,7 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/index.esm';
 import Vue3Toasity from 'vue3-toastify';
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
